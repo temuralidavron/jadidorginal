ba9c091587882c1c609a453cd9739851e1d43604
Abdullox <abdullox@mac-250-g8-notebook-pc.mshome.net>
2024-01-16 19:49:23 +0500

    edit allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h0p&3so%)!_=1n3mi0+a$$!162ru92^xh65l2l4nz^jy^x=&-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['farxod88.pythonanywhere.com',]
 
 # Application definition
 
