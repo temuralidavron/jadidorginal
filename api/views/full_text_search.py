0187989fca727e609f35a07c3aa4721952175d9b
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-01-24 20:12:51 +0500

    search

--- api/views/full_text_search.py
+++ api/views/full_text_search.py
@@ -10,11 +10,11 @@ def full_text_search(request):
     data = request.GET.get('search', 'salom dunyo')
 
     return_data = {}
-    objs_slider = Slayder.objects.filter(title__icontains=data)
-    objs_yangi = Yangiliklar.objects.filter(title__icontains=data)
+    objs_slider = Slayder.objects.filter(title__icontains=data).values()
+    objs_yangi = Yangiliklar.objects.filter(title__icontains=data).values()
 
     return_data['slider'] = objs_slider
     return_data['yangi'] = objs_yangi
 
 
-    return Response(return_data)
+    return Response(data=return_data)
