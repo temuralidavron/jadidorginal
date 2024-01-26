a7ab47d6779a857fab1e4cf512394871c6750238
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-01-26 10:39:30 +0500

    serializers edit

--- jadidlar/serializers.py
+++ jadidlar/serializers.py
@@ -21,51 +21,51 @@ class JadidSerializer(serializers.ModelSerializer):
         tadqiqotlar = instance.tadqiqotlar.all()
         sherlar = instance.sherlar.all()
         hotiralar = instance.hotiralar.all()
+
         if hikmatli_sozlarlar:
             data['hikmatli_sozlar'] = Hikmatli_sozlarSerializer(hikmatli_sozlarlar, many=True).data
 
         if asarlar:
             data['asarlar'] = AsarlarSerializer(asarlar, many=True).data
+            if 'asarlar' in data:
+                for asar in data['asarlar']:
+                    if 'file' in asar and asar['file']:
+                        asar['file'] = f"http://127.0.0.1:8000{asar['file']}"
+                    if 'image' in asar and asar['image']:
+                        asar['image'] = f"http://127.0.0.1:8000{asar['image']}"
 
         if maqolalar:
             data['maqolalar'] = MaqolalarSerializer(maqolalar, many=True).data
+            if 'maqolalar' in data:
+                for maqola in data['maqolalar']:
+                    if 'file' in maqola and maqola['file']:
+                        maqola['file'] = f"http://127.0.0.1:8000{maqola['file']}"
+                    if 'image' in maqola and maqola['image']:
+                        maqola['image'] = f"http://127.0.0.1:8000{maqola['image']}"
 
         if tadqiqotlar:
             data['tadqiqotlar'] = TadqiqotlarSerializer(tadqiqotlar, many=True).data
+            if 'tadqiqotlar' in data:
+                for tadqiqot in data['tadqiqotlar']:
+                    if 'file' in tadqiqot and tadqiqot['file']:
+                        tadqiqot['file'] = f"http://127.0.0.1:8000{tadqiqot['file']}"
+                    # Add more fields as needed
 
         if sherlar:
             data['sherlar'] = SherlarSerializer(sherlar, many=True).data
+            if 'sherlar' in data:
+                for sher in data['sherlar']:
+                    if 'file' in sher and sher['file']:
+                        sher['file'] = f"http://127.0.0.1:8000{sher['file']}"
 
         if hotiralar:
             data['hotiralar'] = HotiralarSerializer(hotiralar, many=True).data
+            if 'hotiralar' in data:
+                for hotira in data['hotiralar']:
+                    if 'file' in hotira and hotira['file']:
+                        hotira['file'] = f"http://127.0.0.1:8000{hotira['file']}"
 
         if images:
-            request = self.context.get('request')
-            data['images'] = [{'image': request.build_absolute_uri(img.image.url)} for img in images]
-
-        if 'asarlar' in data:
-            for asar in data['asarlar']:
-                if 'file' in asar and asar['file']:
-                    asar['file'] = request.build_absolute_uri(asar['file'])
-
-        if 'maqolalar' in data:
-            for maqola in data['maqolalar']:
-                if 'file' in maqola and maqola['file']:
-                    maqola['file'] = request.build_absolute_uri(maqola['file'])
-
-        if 'tadqiqotlar' in data:
-            for tadqiqot in data['tadqiqotlar']:
-                if 'file' in tadqiqot and tadqiqot['file']:
-                    tadqiqot['file'] = request.build_absolute_uri(tadqiqot['file'])
-
-        if 'sherlar' in data:
-            for sher in data['sherlar']:
-                if 'file' in sher and sher['file']:
-                    sher['file'] = request.build_absolute_uri(sher['file'])
-
-        if 'hotiralar' in data:
-            for hotira in data['hotiralar']:
-                if 'file' in hotira and hotira['file']:
-                    hotira['file'] = request.build_absolute_uri(hotira['file'])
+            data['images'] = [{'image': f"http://127.0.0.1:8000{img.image.url}"} for img in images]
 
         return data
