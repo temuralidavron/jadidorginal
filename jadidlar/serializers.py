d18dfbfb049c665cb2c500531b432b2c0704edf2
abdullox <git@github.com:abdullox3134/jadidlar.git>
2024-01-27 10:31:58 +0500

    jadidlar serializers edit

--- jadidlar/serializers.py
+++ jadidlar/serializers.py
@@ -13,6 +13,7 @@ class JadidSerializer(serializers.ModelSerializer):
                   'hikmatli_sozlar', 'asarlar', 'maqolalar', 'tadqiqotlar', 'sherlar', 'hotiralar',)
 
     def to_representation(self, instance):
+        request = self.context.get('request')
         data = super().to_representation(instance)
         images = instance.jadid_images.all()
         hikmatli_sozlarlar = instance.hikmatli_sozlar.all()
@@ -22,6 +23,8 @@ class JadidSerializer(serializers.ModelSerializer):
         sherlar = instance.sherlar.all()
         hotiralar = instance.hotiralar.all()
 
+        base_url = request.build_absolute_uri('/')[:-1]
+
         if hikmatli_sozlarlar:
             data['hikmatli_sozlar'] = Hikmatli_sozlarSerializer(hikmatli_sozlarlar, many=True).data
 
@@ -30,48 +33,47 @@ class JadidSerializer(serializers.ModelSerializer):
             if 'asarlar' in data:
                 for asar in data['asarlar']:
                     if 'file' in asar and asar['file']:
-                        asar['file'] = f"https://jadidlar.pythonanywhere.com{asar['file']}"
+                        asar['file'] = base_url + asar['file']
                     if 'image' in asar and asar['image']:
-                        asar['image'] = f"https://jadidlar.pythonanywhere.com{asar['image']}"
+                        asar['image'] = base_url + asar['image']
 
         if maqolalar:
             data['maqolalar'] = MaqolalarSerializer(maqolalar, many=True).data
             if 'maqolalar' in data:
                 for maqola in data['maqolalar']:
                     if 'file' in maqola and maqola['file']:
-                        maqola['file'] = f"https://jadidlar.pythonanywhere.com{maqola['file']}"
+                        maqola['file'] = base_url + maqola['file']
                     if 'image' in maqola and maqola['image']:
-                        maqola['image'] = f"https://jadidlar.pythonanywhere.com{maqola['image']}"
+                        maqola['image'] = base_url + maqola['image']
 
         if tadqiqotlar:
             data['tadqiqotlar'] = TadqiqotlarSerializer(tadqiqotlar, many=True).data
             if 'tadqiqotlar' in data:
                 for tadqiqot in data['tadqiqotlar']:
                     if 'file' in tadqiqot and tadqiqot['file']:
-                        tadqiqot['file'] = f"https://jadidlar.pythonanywhere.com{tadqiqot['file']}"
+                        tadqiqot['file'] = base_url + tadqiqot['file']
                     if 'image' in tadqiqot and tadqiqot['image']:
-                        tadqiqot['image'] = f"https://jadidlar.pythonanywhere.com{tadqiqot['image']}"
-                    # Add more fields as needed
+                        tadqiqot['image'] = base_url + tadqiqot['image']
 
         if sherlar:
             data['sherlar'] = SherlarSerializer(sherlar, many=True).data
             if 'sherlar' in data:
                 for sher in data['sherlar']:
                     if 'file' in sher and sher['file']:
-                        sher['file'] = f"https://jadidlar.pythonanywhere.com{sher['file']}"
+                        sher['file'] = base_url + sher['file']
                     if 'image' in sher and sher['image']:
-                        sher['image'] = f"https://jadidlar.pythonanywhere.com{sher['image']}"
+                        sher['image'] = base_url + sher['image']
 
         if hotiralar:
             data['hotiralar'] = HotiralarSerializer(hotiralar, many=True).data
             if 'hotiralar' in data:
                 for hotira in data['hotiralar']:
                     if 'file' in hotira and hotira['file']:
-                        hotira['file'] = f"https://jadidlar.pythonanywhere.com{hotira['file']}"
+                        hotira['file'] = base_url + hotira['file']
                     if 'image' in hotira and hotira['image']:
-                        hotira['image'] = f"https://jadidlar.pythonanywhere.com{hotira['image']}"
+                        hotira['image'] = base_url + hotira['image']
 
         if images:
-            data['images'] = [{'image': f"https://jadidlar.pythonanywhere.com{img.image.url}"} for img in images]
+            data['images'] = [{'image': base_url + img.image.url} for img in images]
 
         return data
