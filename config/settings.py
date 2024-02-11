a6291a8f0e6db0c3cce615a6ef8c68d8f2a4a38e
temur <aliyer.temur95@gmail.com>
2024-02-12 01:13:01 +0500

    all files

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h0p&3so%)!_=1n3mi0+a$$!162ru92^xh65l2l4nz^jy^x=&-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['jadidlar.pythonanywhere.com', '*']
 
